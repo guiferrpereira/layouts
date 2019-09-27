2f9f62e17f8eb43f7ca0f6625214fec983a61895
Guilherme Pereira <guilherme.pereira@readinessit.com>
2019-09-27 10:59:11 +0100

    merge

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _el: ElementRef) { }
 
   ngOnInit() {
+  }
+
+  search(event) {
+  	console.log('trying to find me!', event.target.parentElement.querySelectorAll('div.findme'));
   }
 
 }
